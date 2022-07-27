6dc6dd093e7836bb0fab77f6e278536d9b2788b1
Claudio Miguel Olivera Gonzalez <claudioolivera753@gmail.com>
2022-07-27 00:14:30 -0300

    preparado para empezar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CabezalComponent } from './cabezal/cabezal.component';
+import { FormularioComponent } from './formulario/formulario.component';
+import { IngresosComponent } from './ingresos/ingresos.component';
+import { EgresosComponent } from './egresos/egresos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabezalComponent,
+    FormularioComponent,
+    IngresosComponent,
+    EgresosComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
